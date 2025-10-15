c04e32136b2f00188c9d7f36e644227b03e360a1
zhuyeye2001313-rgb <zhuyeye2001313@gmail.com>
2025-10-15 14:23:30 -0700

    add userView

---
+++ TheSmartKitchen/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  TheSmartKitchen
+//
+//  Created by 朱奕颖 on 2025/10/15.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserView()
+}
