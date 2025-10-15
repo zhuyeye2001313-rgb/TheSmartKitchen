799dd6ee910abf22f364cde8898dee8aac9373a2
zhuyeye2001313-rgb <zhuyeye2001313@gmail.com>
2025-10-15 13:41:29 -0700

    add TableView

--- TheSmartKitchen/TableView.swift
+++ TheSmartKitchen/TableView.swift
@@ -5,3 +5,14 @@
 //  Created by 朱奕颖 on 2025/10/15.
 //
 
+import SwiftUI
+
+struct TableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableView()
+}
