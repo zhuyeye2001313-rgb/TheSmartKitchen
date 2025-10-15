63d0876ecdbddc4cf0a1c2f0c70aaded78636f7b
zhuyeye2001313-rgb <zhuyeye2001313@gmail.com>
2025-10-15 00:48:20 -0700

    Initial Commit

---
+++ TheSmartKitchen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheSmartKitchen
+//
+//  Created by 朱奕颖 on 2025/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSmartKitchen/TheSmartKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSmartKitchenApp.swift
+//  TheSmartKitchen
+//
+//  Created by 朱奕颖 on 2025/10/15.
+//
+
+import SwiftUI
+
+@main
+struct TheSmartKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
