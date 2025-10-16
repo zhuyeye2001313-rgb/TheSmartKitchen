df7de3bad9d7bb72fea29d0c89425868e8db0247
zhuyeye2001313-rgb <zhuyeye2001313@gmail.com>
2025-10-15 23:11:18 -0700

    add recipe view

---
+++ TheSmartKitchen/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  TheSmartKitchen
+//
+//  Created by 朱奕颖 on 2025/10/15.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
